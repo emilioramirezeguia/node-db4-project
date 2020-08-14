80adc918c08f3bc1f4a9156e39fd0fc5c95339f8
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-13 18:51:44 -0700

    Set up recipes router

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const recipesRouter = require("./recipes/recipesRouter");
 
 const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "It's working... it's working!" });
+});
+
+server.use("/recipes", recipesRouter);
 
 const port = 8000;
 server.listen(port, () => {
